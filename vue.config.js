cf32cb04d5a2e67514612e2ee2e4659278b3152d
paner28 <61655946+paner28@users.noreply.github.com>
2020-06-30 14:02:15 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
+    pages: {
+        index: {
+              entry: 'src/main.js',
+              template: 'public/index.html',
+              filename: 'index.html',
+              title: 'MQ',
+              chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        subpage: 'src/subpage/main.js'
+    },
     publicPath: '/mq/',
     outputDir: 'docs',
     filenameHashing: false,
     productionSourceMap: false,
     assetsDir: './',
     publicPath: './'
-  }
+}
